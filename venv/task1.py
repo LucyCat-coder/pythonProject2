6c9d2579c6f34efb9d0601c408efb978af271734
Vell <vell1.0@mail.ru>
2021-10-19 22:28:34 +0300

    Initial commit

---
+++ venv/task1.py
@@ -0,0 +1,123 @@
+#001
+# name = input("Enter your name: ")
+# print("Hello", name)
+
+#002
+# fullName = input("Enter your full name: ")
+# print("Hello", fullName)
+
+#003
+# print("What do you call a bear with no teeth?\nA gummy bear!")
+
+#004
+# num1 = int(input("Enter number one: "))
+# num2 = int(input("Enter number two: "))
+# print("The sum of the numbers is", num1+num2)
+
+#005
+# num1 = int(input("Enter number one: "))
+# num2 = int(input("Enter number two: "))
+# num3 = int(input("Enter number three: "))
+# print("Answer:", (num1+num2)*num3)
+
+#006
+# allPizza = int(input("How many slices of pizza did you have? "))
+# eatPizza = int(input("How many slices of pizza have you eaten? "))
+# print("You have", allPizza-eatPizza, "slices of pizza left")
+
+#007
+# name= input("What is your name? ")
+# age = int(input("How old are you? "))
+# print(name, "next birthtday you will be", age+1)
+
+#008
+# bill = int(input("What's your lunch bill? "))
+# people = int(input("How many people were at the meeting? "))
+# print("Each of you has to pay $", bill/people)
+
+#009
+# days = int(input("How many days were that? "))
+# print("Hour:", days*24, "\nMinutes:", days*24*60, "\nSeconds:", days*24*60*60)
+
+#010
+# humanWeight = int(input("Enter your weight in kilograms "))
+# print("Your weight in pounds is ", humanWeight*2,204)
+
+#011
+# num1 = int(input("Please enter a number greater than 100: "))
+# num2 = int(input("Please enter a number less than 10: "))
+# print("The number", num2, "fits in the number", num1, num1//num2, "times")
+
+#012
+# num1 = int(input("Enter first number "))
+# num2 = int(input("Enter second number "))
+# if num1>num2:
+#     print(num2)
+# else:
+#     print(num1)
+
+#013
+# num = int(input("Please enter a number less than 20 "))
+# if num>=20:
+#     print("Too high")
+# else:
+#     print("Thank you")
+
+#014
+# num = int(input("Please enter a number between 10 and 20 inclusive "))
+# if num>=10 and num<=20:
+#     print("Thank you")
+# else:
+#     print("Incorrect answer")
+
+#015
+# color = input("Enter your favorite color ")
+# if str.lower(color)=="red":
+#     print("I like red too")
+# else:
+#     print("I don't like ", color, ".", " I prefer red", sep="")
+
+#016
+# questionOne = input("It's raining? ")
+# questionTwo = input("Is it windy? ")
+# if str.lower(questionOne)=="yes" and str.lower(questionTwo)=="yes":
+#     print("Is it too windy for an umbrella")
+# elif str.lower(questionOne)=="yes" and str.lower(questionTwo)=="no":
+#     print("Take an umbrella")
+# elif str.lower(questionOne)=="no" and str.lower(questionTwo)=="yes":
+#     print("Dress warmly ")
+# else:
+#     print("Enjoy your day")
+
+#017
+# age = int(input("How old are you? "))
+# if age>=18:
+#     print("You can vote")
+# elif age==17:
+#     print("You can learn to drive")
+# elif age==16:
+#     print("You can buy a lottery ticket")
+# else:
+#     print("You can go Trick-or-Treating")
+
+#018
+# num = int(input("Please enter a number "))
+# if num<10:
+#     print("Too low")
+# elif num>=10 and num<=20:
+#     print("Correct")
+# else:
+#     print("Too high")
+
+#019
+# num = int(input("Please enter 1, 2 or 3: "))
+# if num==1:
+#     print("Thank you")
+# elif num==2:
+#     print("Well done")
+# elif num==3:
+#     print("Correct")
+# else:
+#     print("Error message")
+
+#020
